dd53f42f7d3ff92cb1b6cde422b1030e32b09d5d
Alex Shumilov <ashumilov@pivotal.io>
2018-01-31 11:30:50 -0500

    Fix dashboard integration

--- account-facade/src/main/java/io/microsamples/cache/facade/AccountFacadeApplication.java
+++ account-facade/src/main/java/io/microsamples/cache/facade/AccountFacadeApplication.java
@@ -2,10 +2,11 @@ package io.microsamples.cache.facade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableHystrix
+@EnableCircuitBreaker
 public class AccountFacadeApplication {
 
 	public static void main(String[] args) {
